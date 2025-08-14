83f315ee38c0ad198b8a27e6b59f6af634c51d4f
lexlam1524 <lamkachun1524@gmail.com>
2025-08-15 02:11:28 +1000

    update

--- scripts/global/data_types.gd
+++ scripts/global/data_types.gd
@@ -3,6 +3,7 @@ class_name DataTypes
 enum Tools{
 	None,
 	AxeWood,
+	TillGround,
 	WaterCrops,
 	PlantCorn,
 	PlantTomato
--- senses/characters/players/idle_state.gd
+++ senses/characters/players/idle_state.gd
@@ -26,9 +26,12 @@ func _on_next_transitions() -> void:
 		
 	if player.current_tool == DataTypes.Tools.AxeWood && GameInputEvent.use_tool():
 		transition.emit("Chopping")
+		
+	if player.current_tool == DataTypes.Tools.TillGround && GameInputEvent.use_tool():
+		transition.emit("Tilling")
 	
-	if player.current_tool == DataTypes.Tools.AxeWood && GameInputEvent.use_tool():
-		transition.emit("Chopping")
+	if player.current_tool == DataTypes.Tools.WaterCrops && GameInputEvent.use_tool():
+		transition.emit("Watering")
 
 func _on_enter() -> void:
 	pass
